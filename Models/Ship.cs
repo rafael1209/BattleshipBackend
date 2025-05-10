88cdb2cd03ede43c67c74091592a33a8807506cb
rafael1209 <mr.chasmanrafael@gmail.com>
2025-05-10 23:52:11 +0300

    Add Ship and Position classes with Rotation enum
    Introduced a new namespace `BattleshipBackend.Models` to organize code.
    Added the `Ship` class with a `Start` property of type `Position`.
    Created the `Position` class with `X` and `Y` properties for coordinates.
    Included an enumeration `Rotation` to define ship orientation options: `Horizontal` and `Vertical`.

---
+++ Models/Ship.cs
@@ -0,0 +1,18 @@
+﻿namespace BattleshipBackend.Models;
+
+public class Ship
+{
+    public Position Start { get; set; }
+}
+
+public class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
+public enum Rotation
+{
+    Horizontal = 1,     
+    Vertical
+}
